ac253e17f6f5f9217794068817b99c07d7cdcc52
Bouger <gaohe127@126.com>
2017-06-05 23:27:16 +0800

    第一版
    测试测试

---
+++ vueproject-first/src/components/firstVue.vue
@@ -0,0 +1,17 @@
+<template id="">
+  <div class="" id="app">
+    <simple-components></simple-components>
+  </div>
+</template>
+<style media="screen">
+
+</style>
+<script type="text/javascript">
+  import simple from './simple.vue'
+  export default{
+    name:'app',
+    components:{
+      simpleComponents:simple
+    }
+  }
+</script>
---
+++ vueproject-first/src/components/simple.vue
@@ -0,0 +1,9 @@
+<template id="">
+  <p>this is secondVue</p>
+</template>
+<style media="screen">
+
+</style>
+<script type="text/javascript">
+ 
+</script>
